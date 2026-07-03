ff6571b4786c2555b8eced7523a88dd7ee6a47ef
Nakul1009 <nakul10905@gmail.com>
2026-07-03 18:30:19 +0530

    test: building base archintecture, and testing llm calling

---
+++ suproc_agent/llm_client.py
@@ -0,0 +1,20 @@
+from dotenv import load_dotenv
+import os
+from huggingface_hub import InferenceClient
+load_dotenv()
+def llm_call(input:str):
+    client = InferenceClient(
+        api_key=os.getenv("HF_TOKEN"),
+    )
+
+    completion = client.chat.completions.create(
+        model="Qwen/Qwen3-4B-Instruct-2507:nscale",
+    messages=[
+        {
+            "role": "user",
+            "content": input
+        }
+    ],
+)
+
+    print(completion.choices[0].message)
---
+++ suproc_agent/main.py
@@ -0,0 +1,3 @@
+from llm_client import llm_call
+
+llm_call("what is the capital of france")
