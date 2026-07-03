0828a82e675bf6cce9d42911d784ec1bfb39ffed
Nakul1009 <nakul10905@gmail.com>
2026-07-03 21:28:08 +0530

    basic setup done

--- llm_client.py
+++ llm_client.py
@@ -1,20 +1,20 @@
-from dotenv import load_dotenv
 import os
 from huggingface_hub import InferenceClient
+from dotenv import load_dotenv
 load_dotenv()
-def llm_call(input:str):
+def llm_call(input: str):
     client = InferenceClient(
         api_key=os.getenv("HF_TOKEN"),
     )
 
     completion = client.chat.completions.create(
         model="Qwen/Qwen3-4B-Instruct-2507:nscale",
-    messages=[
-        {
-            "role": "user",
-            "content": input
-        }
-    ],
-)
+        messages=[
+            {
+                "role": "user",
+                "content": input
+            }
+        ],
+    )
 
     print(completion.choices[0].message)
